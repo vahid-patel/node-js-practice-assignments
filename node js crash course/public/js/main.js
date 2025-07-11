74b90dae5bee892184f090ccb9658365bd9bcb05
vahid-patel <144371170+vahid-patel@users.noreply.github.com>
2025-07-11 15:53:18 +0530

    completed

--- node js crash course/public/js/main.js
+++ node js crash course/public/js/main.js
@@ -1,5 +1,6 @@
-const output = document.querySelector('output')
-const button = document.querySelector('get-posts-btn')
+const output = document.querySelector('#output')
+const button = document.querySelector('#get-posts-btn')
+const form = document.querySelector('#add-post-form')
 
 
 async function showPosts() {
@@ -12,7 +13,7 @@ async function showPosts() {
     const posts = await res.json()
     output.innerHTML = ''
 
-    posts.array.forEach(post => {
+    posts.forEach(post => {
         const postEl = document.createElement('div')
         postEl.textContent = post.title
         output.appendChild(postEl)
@@ -22,5 +23,39 @@ async function showPosts() {
    }
 }
 
+//Submit new Post
+async function addPost(e)
+{
+    e.preventDefault()
+    const formData = new FormData(this)
+    const title = formData.get('title')
+
+    try{
+        const res = await fetch('http://localhost:3000/apis/posts', {
+            method : 'POST',
+            headers : {
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify({title})
+        })
+        
+        if(!res.ok){
+            throw new Error('Failed to add Post')
+        }
+
+        const newPost = await res.json();
+
+        const postEl = document.createElement('div')
+        postEl.textContent = newPost.title
+        output.appendChild(postEl)
+        showPosts()
+
+    }
+    catch(error){
+        console.error('Error adding post')
+    }
+}
+
 //Event Listener
 button.addEventListener('click',showPosts)
+form.addEventListener('submit',addPost)
